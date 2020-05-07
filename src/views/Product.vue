003fe3833a6730d017211a024d8acc9d831911fe
supardi9512 <supardi9512@gmail.com>
2020-05-08 03:34:58 +0700

    convert html ke vue js product screen part 1 (router product)

---
+++ src/views/Product.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="product">
+    <HeaderShayna />
+   
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Product',
+  components: {
+    HeaderShayna,
+    
+    FooterShayna,
+  }
+}
+</script>
