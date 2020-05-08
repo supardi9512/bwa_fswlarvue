1ff405ae9b9bae3ca51bb198ce5538c527b145da
supardi9512 <supardi9512@gmail.com>
2020-05-09 02:32:30 +0700

    convert html ke vue js product screen part 8 (shopping cart)

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ Vue.use(VueRouter)
     path: '/product',
     name: 'Product',
     component: Product
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: ShoppingCart
   },
 ]
 
