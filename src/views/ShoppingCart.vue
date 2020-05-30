965dbe09a4b0014ed9f12b203220f4f588e802ee
supardi9512 <supardi9512@gmail.com>
2020-05-31 01:29:28 +0700

    keranjang localStorage Part 10 (membuat fungsi checkout)

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -58,19 +58,19 @@
                                     <form>
                                         <div class="form-group">
                                             <label for="namaLengkap">Nama lengkap</label>
-                                            <input type="text" class="form-control" id="namaLengkap" aria-describedby="namaHelp" placeholder="Masukan Nama">
+                                            <input type="text" class="form-control" id="namaLengkap" aria-describedby="namaHelp" placeholder="Masukan Nama" v-model="customerInfo.name">
                                         </div>
                                         <div class="form-group">
                                             <label for="namaLengkap">Email Address</label>
-                                            <input type="email" class="form-control" id="emailAddress" aria-describedby="emailHelp" placeholder="Masukan Email">
+                                            <input type="email" class="form-control" id="emailAddress" aria-describedby="emailHelp" placeholder="Masukan Email" v-model="customerInfo.email">
                                         </div>
                                         <div class="form-group">
                                             <label for="namaLengkap">No. HP</label>
-                                            <input type="text" class="form-control" id="noHP" aria-describedby="noHPHelp" placeholder="Masukan No. HP">
+                                            <input type="text" class="form-control" id="noHP" aria-describedby="noHPHelp" placeholder="Masukan No. HP" v-model="customerInfo.number">
                                         </div>
                                         <div class="form-group">
                                             <label for="alamatLengkap">Alamat Lengkap</label>
-                                            <textarea class="form-control" id="alamatLengkap" rows="3"></textarea>
+                                            <textarea class="form-control" id="alamatLengkap" rows="3" v-model="customerInfo.address"></textarea>
                                         </div>
                                     </form>
                                 </div>
@@ -90,7 +90,7 @@
                                         <li class="subtotal mt-3">No. Rekening <span>2208 1996 1403</span></li>
                                         <li class="subtotal mt-3">Nama Penerima <span>Shayna</span></li>
                                     </ul>
-                                    <router-link to="/success" class="proceed-btn">I ALREADY PAID</router-link>
+                                    <a @click="checkout()" class="proceed-btn">I ALREADY PAID</a>
                                 </div>
                             </div>
                         </div>
@@ -104,6 +104,7 @@
 
 <script>
 import HeaderShayna from '@/components/HeaderShayna.vue';
+import axios from 'axios';
 
 export default {
     name: 'ShoppingCart',
@@ -112,10 +113,39 @@ export default {
     },
     data() {
         return {
-            keranjangUser: []
+            keranjangUser: [],
+            customerInfo: {
+                name: '',
+                email: '',
+                number: '',
+                address: ''
+            }
         }
     },
     methods: {
+        // fungsi mengirim data ke API
+        checkout() {
+            let productIds = this.keranjangUser.map(function(product) {
+                return product.id;
+            });
+
+            let checkoutData = {
+                'name': this.customerInfo.name,
+                'email': this.customerInfo.email,
+                'number': this.customerInfo.number,
+                'address': this.customerInfo.address,
+                'transaction_total': this.totalBiaya,
+                'transaction_status': 'PENDING',
+                'transaction_details': productIds
+            };
+
+            axios
+                .post('http://127.0.0.1:8000/api/checkout', checkoutData)
+                .then(() => this.$router.push('success'))
+                // komentar di bawah ini untuk menonaktifkan eslint, agar dapat menggunakan console log id
+                // eslint-disable-next-line no-console
+                .catch(err => console.log(err));
+        },
         removeItem(index) {
             // menghapus data
             this.keranjangUser.splice(index, 1);
